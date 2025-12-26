4062ef2d4c376d1a25d9cb5d94fa7c8e545075eb
Phuc Tran <glup3.tran@gmail.com>
2025-12-26 21:04:33 +0100

    add whisper & expo audio

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,48 @@
 import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { Alert, Button, Platform, StyleSheet, Text } from 'react-native';
 
 import { HelloWave } from '@/components/hello-wave';
 import ParallaxScrollView from '@/components/parallax-scroll-view';
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
-import { Link } from 'expo-router';
+import { initWhisper, WhisperContext } from 'whisper.rn/index.js'
+import { useEffect, useState } from 'react';
+import {
+  useAudioRecorder,
+  AudioModule,
+  RecordingPresets,
+  setAudioModeAsync,
+  useAudioRecorderState,
+} from 'expo-audio';
 
 export default function HomeScreen() {
+  const [whisperContext, setWhisperContext] = useState<WhisperContext | null>(
+    null
+  );
+  const [permissionsGranted, setPermissionsGranted] = useState(false)
+
+  const initializeModel = async () => {
+    try {
+      const context = await initWhisper({
+        filePath: require('@/assets/whisper-models/ggml-tiny-q8_0.bin')
+      })
+      setWhisperContext(context)
+    } catch (error) {
+      console.error('failed model init:', error)
+    }
+  }
+
+  useEffect(() => {
+    initializeModel()
+  }, [])
+
+  useEffect(() => {
+    (async () => {
+      const status = await AudioModule.getRecordingPermissionsAsync()
+      setPermissionsGranted(status.granted)
+    })()
+  }, [])
+
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
@@ -18,61 +53,21 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">Hi Anna!</ThemedText>
         <HelloWave />
       </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <Link href="/modal">
-          <Link.Trigger>
-            <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-          </Link.Trigger>
-          <Link.Preview />
-          <Link.Menu>
-            <Link.MenuAction title="Action" icon="cube" onPress={() => alert('Action pressed')} />
-            <Link.MenuAction
-              title="Share"
-              icon="square.and.arrow.up"
-              onPress={() => alert('Share pressed')}
-            />
-            <Link.Menu title="More" icon="ellipsis">
-              <Link.MenuAction
-                title="Delete"
-                icon="trash"
-                destructive
-                onPress={() => alert('Delete pressed')}
-              />
-            </Link.Menu>
-          </Link.Menu>
-        </Link>
-
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
+      <ThemedView>
+        <Button title="Permissions" onPress={async () => {
+          const status = await AudioModule.requestRecordingPermissionsAsync()
+          if (!status.granted) {
+            Alert.alert('Permission to access microphone was denied')
+          }
+          setAudioModeAsync({
+            playsInSilentMode: true,
+            allowsRecording: true,
+          })
+        }} />
+        <Text className='text-red-500'>Permissions {permissionsGranted ? 'granted' : 'denied'}</Text>
       </ThemedView>
     </ParallaxScrollView>
   );
